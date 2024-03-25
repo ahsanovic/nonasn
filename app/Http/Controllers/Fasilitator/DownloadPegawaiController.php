8997726eed2beae9dc1bf345a3012238ea67969a
ahsan <ahsanovic_7@yahoo.com>
2024-03-25 15:30:24 +0700

    penambahan kolom usia saat download

--- app/Http/Controllers/Fasilitator/DownloadPegawaiController.php
+++ app/Http/Controllers/Fasilitator/DownloadPegawaiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Fasilitator;
 
 use Hashids\Hashids;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Rap2hpoutre\FastExcel\FastExcel;
 use App\Models\Fasilitator\DownloadPegawai;
@@ -26,11 +27,22 @@ class DownloadPegawaiController extends Controller
             $hashid = $this->_hashId();
             if (auth()->user()->level == 'admin') {
                 if (!$request->query('nama')) {
-                    $pegawai = DownloadPegawai::whereAktif('Y')
+                    // $pegawai = DownloadPegawai::whereAktif('Y')
+                    //         ->where('id_skpd', 'like', $hashid->decode($request->segment(3))[0] . '%')
+                    //         ->get();
+                    $pegawai = DB::table('download')
+                            ->select('*', DB::raw('timestampdiff(year, tgl_lahir, curdate()) as usia'))
                             ->where('id_skpd', 'like', $hashid->decode($request->segment(3))[0] . '%')
+                            ->where('aktif', 'Y')
                             ->get();
                 } else {
-                    $pegawai = DownloadPegawai::whereAktif('Y')
+                    // $pegawai = DownloadPegawai::whereAktif('Y')
+                    //         ->where('nama', 'like', '%' . $request->query('nama') . '%')
+                    //         ->orWhere('niptt', $request->query('nama'))
+                    //         ->get();
+                    $pegawai = DB::table('download')
+                            ->select('*', DB::raw('timestampdiff(year, tgl_lahir, curdate()) as usia'))
+                            ->where('aktif', 'Y')
                             ->where('nama', 'like', '%' . $request->query('nama') . '%')
                             ->orWhere('niptt', $request->query('nama'))
                             ->get();
@@ -49,6 +61,7 @@ class DownloadPegawaiController extends Controller
             }
             return (new FastExcel($pegawai))->download('data-pegawai.xlsx');
         } catch (\Throwable $th) {
+            throw $th;
             return back()->with(["type" => "error", "message" => "terjadi kesalahan!"]);
         }
     }
@@ -83,16 +96,32 @@ class DownloadPegawaiController extends Controller
         try {
             $idSkpd = $request->idSkpd ?? auth()->user()->id_skpd;
             if (auth()->user()->level == 'admin') {
-                $pegawai = DownloadPegawai::whereAktif('Y')
-                            ->where('id_skpd', 'like', $idSkpd . '%')
-                            ->get();
+                // $pegawai = DownloadPegawai::whereAktif('Y')
+                //             ->where('id_skpd', 'like', $idSkpd . '%')
+                //             ->get();                
+                $pegawai = DB::table('download')
+                        ->select('*', DB::raw('timestampdiff(year, tgl_lahir, curdate()) as usia'))
+                        ->where('id_skpd', 'like', $idSkpd . '%')
+                        ->where('aktif', 'Y')
+                        ->get();
             } else {
-                $pegawai = DownloadPegawai::whereAktif('Y')
+                // $pegawai = DownloadPegawai::whereAktif('Y')
+                //             ->where('id_skpd', 'like', $idSkpd . '%')
+                //             ->get()->makeHidden(['tahun_penilaian', 'rekomendasi']);
+                $pegawai = DB::table('download')
+                            ->select('*', DB::raw('timestampdiff(year, tgl_lahir, curdate()) as usia'))
                             ->where('id_skpd', 'like', $idSkpd . '%')
-                            ->get()->makeHidden(['tahun_penilaian', 'rekomendasi']);
+                            ->where('aktif', 'Y')
+                            ->get()
+                            ->map(function($peg) {
+                                unset($peg->tahun_penilaian);
+                                unset($peg->rekomendasi);
+                                return $peg;
+                            });
             }
             return (new FastExcel($pegawai))->download("data-pegawai-all.xlsx");
         } catch (\Throwable $th) {
+            // throw $th;
             return back()->with(["type" => "error", "message" => "terjadi kesalahan!"]);
         }
     }
@@ -101,9 +130,15 @@ class DownloadPegawaiController extends Controller
     {
         try {
             $idSkpd = $request->idSkpd ?? auth()->user()->id_skpd;
-            $pegawai = DownloadPegawai::whereJenis_ptt('PTT-PK')
+            // $pegawai = DownloadPegawai::whereJenis_ptt('PTT-PK')
+            //             ->where('id_skpd', 'like', $idSkpd . '%')
+            //             ->whereAktif('Y')
+            //             ->get();
+            $pegawai = DB::table('download')
+                        ->select('*', DB::raw('timestampdiff(year, tgl_lahir, curdate()) as usia'))
+                        ->where('jenis_ptt', 'PTT-PK')
                         ->where('id_skpd', 'like', $idSkpd . '%')
-                        ->whereAktif('Y')
+                        ->where('aktif', 'Y')
                         ->get();
             return (new FastExcel($pegawai))->download("data-pegawai-pttpk.xlsx");
         } catch (\Throwable $th) {
@@ -115,9 +150,15 @@ class DownloadPegawaiController extends Controller
     {
         try {
             $idSkpd = $request->idSkpd ?? auth()->user()->id_skpd;
-            $pegawai = DownloadPegawai::whereJenis_ptt('PTT CABDIN')
+            // $pegawai = DownloadPegawai::whereJenis_ptt('PTT CABDIN')
+            //             ->where('id_skpd', 'like', $idSkpd . '%')
+            //             ->whereAktif('Y')
+            //             ->get();
+            $pegawai = DB::table('download')
+                        ->select('*', DB::raw('timestampdiff(year, tgl_lahir, curdate()) as usia'))
+                        ->where('jenis_ptt', 'PTT CABDIN')
                         ->where('id_skpd', 'like', $idSkpd . '%')
-                        ->whereAktif('Y')
+                        ->where('aktif', 'Y')
                         ->get();
             return (new FastExcel($pegawai))->download("data-pegawai-ptt-cabdin.xlsx");
         } catch (\Throwable $th) {
@@ -129,9 +170,15 @@ class DownloadPegawaiController extends Controller
     {
         try {
             $idSkpd = $request->idSkpd ?? auth()->user()->id_skpd;
-            $pegawai = DownloadPegawai::whereJenis_ptt('PTT SEKOLAH')
+            // $pegawai = DownloadPegawai::whereJenis_ptt('PTT SEKOLAH')
+            //             ->where('id_skpd', 'like', $idSkpd . '%')
+            //             ->whereAktif('Y')
+            //             ->get();
+            $pegawai = DB::table('download')
+                        ->select('*', DB::raw('timestampdiff(year, tgl_lahir, curdate()) as usia'))
+                        ->where('jenis_ptt', 'PTT SEKOLAH')
                         ->where('id_skpd', 'like', $idSkpd . '%')
-                        ->whereAktif('Y')
+                        ->where('aktif', 'Y')
                         ->get();
             return (new FastExcel($pegawai))->download("data-pegawai-ptt-sekolah.xlsx");
         } catch (\Throwable $th) {
@@ -143,9 +190,15 @@ class DownloadPegawaiController extends Controller
     {
         try {
             $idSkpd = $request->idSkpd ?? auth()->user()->id_skpd;
-            $pegawai = DownloadPegawai::whereJenis_ptt('GTT')
+            // $pegawai = DownloadPegawai::whereJenis_ptt('GTT')
+            //             ->where('id_skpd', 'like', $idSkpd . '%')
+            //             ->whereAktif('Y')
+            //             ->get();
+            $pegawai = DB::table('download')
+                        ->select('*', DB::raw('timestampdiff(year, tgl_lahir, curdate()) as usia'))
+                        ->where('jenis_ptt', 'GTT')
                         ->where('id_skpd', 'like', $idSkpd . '%')
-                        ->whereAktif('Y')
+                        ->where('aktif', 'Y')
                         ->get();
             return (new FastExcel($pegawai))->download("data-pegawai-gtt.xlsx");
         } catch (\Throwable $th) {
