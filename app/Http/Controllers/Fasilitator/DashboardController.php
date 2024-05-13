20d7ba80d65e98181f9b1af2dd0394aa109973bc
ahsan <ahsanovic_7@yahoo.com>
2024-05-13 17:00:19 +0700

    menambahkan fitur leaderboard akun fasilitator

--- app/Http/Controllers/Fasilitator/DashboardController.php
+++ app/Http/Controllers/Fasilitator/DashboardController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers\Fasilitator;
 
+use App\Models\HasilSimulasiCpns;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Http;
+use App\Models\HasilSimulasiPppkTeknis;
+use App\Models\HasilSimulasiPppkWawancara;
+use App\Models\HasilSimulasiPppkManajerial;
 
 class DashboardController extends Controller
 {
@@ -15,6 +19,41 @@ class DashboardController extends Controller
         // if ($fetch->failed()) abort(500);
         $response = json_decode($fetch->body());
 
-        return view('fasilitator.dashboard.index', compact('response', 'web_url'));
+        // leaderboard simulasi cpns
+        $hasil_simulasi_cpns = HasilSimulasiCpns::with('pegawai')
+                            ->whereRelation('pegawai', 'id_skpd', 'like', auth()->user()->id_skpd . '%')
+                            ->orderByDesc('nilai_total')
+                            ->limit(10)
+                            ->get();
+
+        // leaderboard simulasi pppk teknis
+        $hasil_simulasi_teknis = HasilSimulasiPppkTeknis::with('pegawai')
+                            ->whereRelation('pegawai', 'id_skpd', 'like', auth()->user()->id_skpd . '%')
+                            ->orderByDesc('nilai_total')
+                            ->limit(10)
+                            ->get();
+        
+        // leaderboard simulasi pppk manajerial
+        $hasil_simulasi_manajerial = HasilSimulasiPppkManajerial::with('pegawai')
+                            ->whereRelation('pegawai', 'id_skpd', 'like', auth()->user()->id_skpd . '%')
+                            ->orderByDesc('nilai_total')
+                            ->limit(10)
+                            ->get();
+        
+        // leaderboard simulasi pppk wawancara
+        $hasil_simulasi_wawancara = HasilSimulasiPppkWawancara::with('pegawai')
+                            ->whereRelation('pegawai', 'id_skpd', 'like', auth()->user()->id_skpd . '%')
+                            ->orderByDesc('nilai_total')
+                            ->limit(10)
+                            ->get();
+
+        return view('fasilitator.dashboard.index', compact(
+            'response',
+            'web_url',
+            'hasil_simulasi_cpns',
+            'hasil_simulasi_teknis',
+            'hasil_simulasi_manajerial',
+            'hasil_simulasi_wawancara'
+        ));
     }
 }
--- resources/views/fasilitator/dashboard/index.blade.php
+++ resources/views/fasilitator/dashboard/index.blade.php
@@ -5,6 +5,187 @@
         </div>
     </x-page-header>
     <div class="row">
+        <div class="col-md-12 col-lg-6 col-xl-5">
+            <div class="main-card mb-3 card">
+                <div class="card-header">
+                    <i class="header-icon lnr-license icon-gradient bg-love-kiss"> </i>Leaderboard Simulasi CPNS
+                </div>
+                <div class="card-body">
+                    <div class="widget-title opacity-5 text-muted text-uppercase mb-3">TOP TEN LIST</div>
+                    <ul class="rm-list-borders rm-list-borders-scroll list-group list-group-flush">
+                        @php
+                            $position = 1;
+                        @endphp
+                        @foreach ($hasil_simulasi_cpns as $item)
+                            <li class="list-group-item">
+                                <div class="widget-content p-0">
+                                    <div class="widget-content-wrapper">
+                                        <div class="widget-content-left mr-3">
+                                            <img width="38" class="rounded-circle" src="{{ route('pegawai.image', ['image' => rtrim($item->pegawai->foto)]) }}" alt="">
+                                        </div>
+                                        <div class="widget-content-left">
+                                            <div class="widget-heading">{{ $item->pegawai->nama }}</div>
+                                            <div class="widget-subheading">
+                                                @php
+                                                    $skpd = substr($item->pegawai->id_skpd, 0, 3);
+                                                @endphp
+                                                {{ Str::title(App\Models\Skpd::whereId($skpd)->first()->name) }}
+                                            </div>
+                                        </div>
+                                        <div class="widget-content-right">
+                                            <div class="font-size-xlg text-muted">
+                                                @if ($position <= 3)
+                                                <small class="opacity-5 pr-1"><i class="pe-7s-cup"></i></small>
+                                                @endif
+                                                <span>{{ $item->nilai_total }}</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            @php
+                                $position = $position + 1;
+                            @endphp
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-12 col-lg-6 col-xl-5">
+            <div class="main-card mb-3 card">
+                <div class="card-header">
+                    <i class="header-icon lnr-license icon-gradient bg-happy-itmeo"> </i> Leaderboard Simulasi PPPK
+                    <div class="btn-actions-pane-right actions-icon-btn">
+                        <div role="group" class="btn-group-sm nav btn-group">
+                            <a data-toggle="tab" href="#tab-eg3-0" class="btn-shadow btn btn-success active show">Teknis</a>
+                            <a data-toggle="tab" href="#tab-eg3-1" class="btn-shadow btn btn-success show">Manajerial</a>
+                            <a data-toggle="tab" href="#tab-eg3-2" class="btn-shadow btn btn-success show">Wawancara</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="tab-content">
+                        <div class="widget-title opacity-5 text-muted text-uppercase mb-3">TOP TEN LIST</div>
+                        <div class="tab-pane show active" id="tab-eg3-0" role="tabpanel">
+                            <ul class="rm-list-borders rm-list-borders-scroll list-group list-group-flush">
+                            @php
+                                $position = 1;
+                            @endphp
+                            @foreach ($hasil_simulasi_teknis as $item)
+                                <li class="list-group-item">
+                                    <div class="widget-content p-0">
+                                        <div class="widget-content-wrapper">
+                                            <div class="widget-content-left mr-3">
+                                                <img width="38" class="rounded-circle" src="{{ route('nonasn.image', ['image' => rtrim($item->pegawai->foto)]) }}" alt="">
+                                            </div>
+                                            <div class="widget-content-left">
+                                                <div class="widget-heading">{{ $item->pegawai->nama }}</div>
+                                                <div class="widget-subheading">
+                                                    @php
+                                                        $skpd = substr($item->pegawai->id_skpd, 0, 3);
+                                                    @endphp
+                                                    {{ Str::title(App\Models\Skpd::whereId($skpd)->first()->name) }}
+                                                </div>
+                                            </div>
+                                            <div class="widget-content-right">
+                                                <div class="font-size-xlg text-muted">
+                                                    @if ($position <= 3)
+                                                    <small class="opacity-5 pr-1"><i class="pe-7s-cup"></i></small>
+                                                    @endif
+                                                    <span>{{ $item->nilai_total }}</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                @php
+                                    $position = $position + 1;
+                                @endphp
+                            @endforeach
+                            </ul>
+                        </div>
+                        <div class="tab-pane show" id="tab-eg3-1" role="tabpanel">
+                            <ul class="rm-list-borders rm-list-borders-scroll list-group list-group-flush">
+                            @php
+                                $position = 1;
+                            @endphp
+                            @foreach ($hasil_simulasi_manajerial as $item)
+                                <li class="list-group-item">
+                                    <div class="widget-content p-0">
+                                        <div class="widget-content-wrapper">
+                                            <div class="widget-content-left mr-3">
+                                                <img width="38" class="rounded-circle" src="{{ route('nonasn.image', ['image' => rtrim($item->pegawai->foto)]) }}" alt="">
+                                            </div>
+                                            <div class="widget-content-left">
+                                                <div class="widget-heading">{{ $item->pegawai->nama }}</div>
+                                                <div class="widget-subheading">
+                                                    @php
+                                                        $skpd = substr($item->pegawai->id_skpd, 0, 3);
+                                                    @endphp
+                                                    {{ Str::title(App\Models\Skpd::whereId($skpd)->first()->name) }}
+                                                </div>
+                                            </div>
+                                            <div class="widget-content-right">
+                                                <div class="font-size-xlg text-muted">
+                                                    @if ($position <= 3)
+                                                    <small class="opacity-5 pr-1"><i class="pe-7s-cup"></i></small>
+                                                    @endif
+                                                    <span>{{ $item->nilai_total }}</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                @php
+                                    $position = $position + 1;
+                                @endphp
+                            @endforeach
+                            </ul>
+                        </div>
+                        <div class="tab-pane show" id="tab-eg3-2" role="tabpanel">
+                            <ul class="rm-list-borders rm-list-borders-scroll list-group list-group-flush">
+                            @php
+                                $position = 1;
+                            @endphp
+                            @foreach ($hasil_simulasi_wawancara as $item)
+                                <li class="list-group-item">
+                                    <div class="widget-content p-0">
+                                        <div class="widget-content-wrapper">
+                                            <div class="widget-content-left mr-3">
+                                                <img width="38" class="rounded-circle" src="{{ route('nonasn.image', ['image' => rtrim($item->pegawai->foto)]) }}" alt="">
+                                            </div>
+                                            <div class="widget-content-left">
+                                                <div class="widget-heading">{{ $item->pegawai->nama }}</div>
+                                                <div class="widget-subheading">
+                                                    @php
+                                                        $skpd = substr($item->pegawai->id_skpd, 0, 3);
+                                                    @endphp
+                                                    {{ Str::title(App\Models\Skpd::whereId($skpd)->first()->name) }}
+                                                </div>
+                                            </div>
+                                            <div class="widget-content-right">
+                                                <div class="font-size-xlg text-muted">
+                                                    @if ($position <= 3)
+                                                    <small class="opacity-5 pr-1"><i class="pe-7s-cup"></i></small>
+                                                    @endif
+                                                    <span>{{ $item->nilai_total }}</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                @php
+                                    $position = $position + 1;
+                                @endphp
+                            @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
         <div class="col-md-6 col-lg-6">
             <div class="main-card mb-3 card">
                 <div class="card-body"><h5 class="card-title">Berita Terbaru Web BKD Jatim</h5>
